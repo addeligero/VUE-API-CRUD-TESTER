a4c4f3dc2b353ea80b3aa6615cad37c387b0ddc3
ADRIAN <adriandeligero.1030@gmail.com>
2025-02-11 14:06:30 +0800

    login form done

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import register from "../views/Auth/Register.vue";
+import Login from "@/views/Auth/Login.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: "/register",
       name: "register",
       component: register,
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
     },
   ],
 });
---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="fill-height bg-teal-lighten-1" fluid>
+    <v-row justify="center" align="center">
+      <v-col cols="4">
+        <v-card elevation="8">
+          <v-card-title class="text-center">Login</v-card-title>
+          <v-card-text>
+            <LoginInput />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import LoginInput from "@/components/Auth/LoginInput.vue";
+</script>
+
+<style scoped>
+.v-container {
+  min-height: 100vh;
+}
+</style>
